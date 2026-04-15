8c366a64e71cc1a64e9b8690d2dc54f2fb9fa1ad
Monish Shah <tomonishshah@gmail.com>
2026-04-15 23:26:49 +0530

    added access control

--- src/main/java/com/taco/cloud/security/securityConfig.java
+++ src/main/java/com/taco/cloud/security/securityConfig.java
@@ -2,12 +2,13 @@ package com.taco.cloud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class securityConfig {
@@ -23,5 +24,15 @@ public class securityConfig {
             if(user!=null) return user;
             throw new UsernameNotFoundException("User '" + username + "' not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/design", "/orders").hasRole("USER")
+                .anyRequest().permitAll()
+            )
+            .build();
     }
 }
